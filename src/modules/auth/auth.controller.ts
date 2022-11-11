9c4a8a7ad7861796c7fde68673d0e73b70a99f31
Nguyen Trong Luat <55679058+luat111@users.noreply.github.com>
2022-11-11 09:32:04 +0700

    feat: check permission

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,11 +1,13 @@
-import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { Roles } from 'src/core/decorators/role.decorator';
 import { User } from 'src/core/decorators/user.decorator';
 import BadRequest from 'src/core/exceptions/bad-request.exception';
 import { AuthService } from './auth.service';
 import { SignInDTO } from './dto';
 import { JwtAuthGuard } from './guard/jwt.guard';
 import { LocalAuthGuard } from './guard/local.guard';
+import { RolesGuard } from './guard/role.guard';
 import { IStaff } from './interface';
 
 @ApiBearerAuth()
@@ -28,8 +30,20 @@ export class AuthController {
   }
 
   @UseGuards(JwtAuthGuard)
-  @Post('info')
+  @Get('info')
   async getInfo(@User() user: IStaff): Promise<IStaff> {
+    try {
+      return user;
+    } catch (err) {
+      throw new BadRequest(AuthController.name, err);
+    }
+  }
+
+  @Roles('ntltest')
+  @UseGuards(RolesGuard)
+  @UseGuards(JwtAuthGuard)
+  @Get('test')
+  async getabc(@User() user: IStaff): Promise<IStaff> {
     try {
       return user;
     } catch (err) {
